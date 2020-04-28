c364a93cf5dec0017289177beb36b85731000e30
Antoine Tournedouet <antoine.tournedouet@gmail.com>
2020-04-28 17:15:10 +0200

    logo_qui_tourne_weeee

---
+++ client/src/components/main/Connexion.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+class Connexion extends React.Component {
+    render() {
+        return <p>Connexion</p>;
+    }
+}
+
+export default Connexion;
---
+++ client/src/components/main/Home.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+function Home() {
+
+    return (
+        <div>
+            <p>Hello</p>
+        </div>
+    );
+
+}
+
+export default Home;
