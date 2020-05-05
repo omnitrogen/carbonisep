78f9ec3d9b90551ed88ab1723d675b303c743930
Gauthier <39213977+Gog001@users.noreply.github.com>
2020-05-05 16:40:19 +0200

    reparation merge

--- client/src/components/main/static/Login.js
+++ client/src/components/main/static/Login.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
+const API = "http://localhost:8000/";
+
 export default class Login extends Component {
     constructor() {
         super();
--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,12 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
->>>>>>> integration
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById("root")
 );
