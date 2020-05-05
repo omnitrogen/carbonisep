5435da9900061db83685d654352445d3722c02c8
Gauthier <39213977+Gog001@users.noreply.github.com>
2020-05-05 10:17:27 +0200

    test encodage caracters

--- client/src/components/main/static/Home.js
+++ client/src/components/main/static/Home.js
@@ -10,20 +10,20 @@ export default class Home extends Component {
                 <div className="border border-dark rounded w-80 p-4">
                     <h1>CarbonISEP</h1>
                     <h2>Agir maintenant</h2>
-                    <p>CarbonISEP est là pour vous motiver à contribuer à l’écologie, on peut donc vous filer quelques petites astuces pour s’y mettre au quotidien :)
+                    <p>CarbonISEP est lÃ  pour vous motiver Ã  contribuer Ã  l'Ã©cologie, on peut donc vous filer quelques petites astuces pour sï¿½y mettre au quotidien :)
                         <br/>
-                        Il n’y a pas besoin de consacrer sa vie à l’écologie, mais tous les petits gestes comptent
+                        Il nï¿½y a pas besoin de consacrer sa vie ï¿½ lï¿½ï¿½cologie, mais tous les petits gestes comptent
                         METTRE ICI IMAGE PARTIE
                     </p>
                     <h2>Entrez dans le game</h2>
-                    <p>Quoi de mieux qu'un petit jeu entre amis ? Lances la partie dès maintenant en te créant un compte</p>
+                    <p>Quoi de mieux qu'un petit jeu entre amis ? Lances la partie dï¿½s maintenant en te crï¿½ant un compte</p>
                     <JoinOrCreateGame/>
-                    <p>Tu veux juste avoir quelques astuces pour être écolo ? On a quelques <a href="tips">tips</a> pour toi alors ;)
+                    <p>Tu veux juste avoir quelques astuces pour ï¿½tre ï¿½colo ? On a quelques <a href="tips">tips</a> pour toi alors ;)
                         <br/>
-                        CarbonISEP peut également te proposer des solutions écolos sur mesure, il faut juste remplir un petit questionnaire
+                        CarbonISEP peut ï¿½galement te proposer des solutions ï¿½colos sur mesure, il faut juste remplir un petit questionnaire
                     </p>
                     <Button variant="primary" href="connexion" type="submit" className="mt-3">
-                        Connecte-toi pour accéder au questionnaire!
+                        Connecte-toi pour accï¿½der au questionnaire!
                     </Button>
                 </div>
             </div>
