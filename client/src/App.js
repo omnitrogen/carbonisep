b6fb924e730442e8e95338e7177ca24ad2d08d75
Archibald Perez <archieperez99@gmail.com>
2020-05-05 15:36:15 +0200

    Router switch for navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import Navbar1 from './components/templates/nav/Navbar1';
-import Home from './components/main/static/Home';
-import StaticFooter from './components/templates/StaticFooter';
-import ConnectedNavbars from './components/templates/nav/ConnectedNavbars';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Home from "./components/main/static/Home";
+import Tips from "./components/main/static/Tips";
+import Connexion from "./components/main/static/Connexion";
+import ConnectedNavbars from "./components/templates/nav/ConnectedNavbars";
 
 function App() {
     return (
-      <>
-            <ConnectedNavbars/>
-      </>
-  );
+        <div>
+            <ConnectedNavbars />
+            <Router>
+                <Switch>
+                    <Route path="/tips">
+                        <Tips />
+                    </Route>
+                    <Route path="/connexion">
+                        <Connexion />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
