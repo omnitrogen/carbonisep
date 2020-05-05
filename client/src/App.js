cf540d3320c906e73732afcb798e5ffa72eb91f1
Gauthier <39213977+Gog001@users.noreply.github.com>
2020-05-05 16:50:51 +0200

    reparation merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,35 @@
 import React from "react";
-
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap.css";
+import Home from "./components/main/static/Home";
+import Tips from "./components/main/static/Tips";
+import Connexion from "./components/main/static/Connexion";
+import StaticFooter from "./components/templates/StaticFooter";
 import Login from "./components/main/static/Login";
 
 function App() {
     return (
-        <>
+        <div>
             <Login />
-        </>
+            <Router
+                className="position-absolute"
+                style={{ left: "15%", right: "15%", overflow: "hidden" }}
+            >
+                <Switch>
+                    <Route path="/tips">
+                        <Tips />
+                    </Route>
+                    <Route path="/connexion">
+                        <Connexion />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </Router>
+            <StaticFooter />
+        </div>
     );
 }
 
