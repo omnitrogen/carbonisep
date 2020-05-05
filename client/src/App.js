260e139e41e565e88c7218bc523268a8e6f31a7d
Archibald Perez <archieperez99@gmail.com>
2020-05-05 17:54:06 +0200

    Fix footer and main page

--- client/src/App.js
+++ client/src/App.js
@@ -4,30 +4,27 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap.css";
 import Home from "./components/main/static/Home";
 import Tips from "./components/main/static/Tips";
-import Connexion from "./components/main/static/Connexion";
 import StaticFooter from "./components/templates/StaticFooter";
 import Login from "./components/main/static/Login";
 
 function App() {
     return (
         <div>
-            <Login />
-            <Router
-                className="position-absolute"
-                style={{ left: "15%", right: "15%", overflow: "hidden" }}
-            >
-                <Switch>
-                    <Route path="/tips">
-                        <Tips />
-                    </Route>
-                    <Route path="/connexion">
-                        <Connexion />
-                    </Route>
-                    <Route path="/">
-                        <Home />
-                    </Route>
-                </Switch>
-            </Router>
+            <div style={{ minHeight: "90vh" }}>
+                <Router>
+                    <Switch>
+                        <Route path="/tips">
+                            <Tips />
+                        </Route>
+                        <Route path="/connexion">
+                            <Login />
+                        </Route>
+                        <Route path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </Router>
+            </div>
             <StaticFooter />
         </div>
     );
--- client/src/components/templates/StaticFooter.js
+++ client/src/components/templates/StaticFooter.js
@@ -8,21 +8,23 @@ export default class Footer extends React.Component {
         return (
             <footer id="footer" style={{ width: "100%" }}>
                 <div
-                    className="border border-dark w-80 p-4 d-flex justify-content-between"
-                    style={{ backgroundColor: "#88B03C" }}
+                    className="border border-dark w-80 d-flex justify-content-between"
+                    style={{ backgroundColor: "#88B03C", height: "10vh" }}
                 >
                     <Navbar.Brand href="home">
                         <img src={logo} style={{ zoom: 0.05 }} alt="logo" />
                     </Navbar.Brand>
                     <p>
-                        Copyright 2020. Tous droits réservés. En cas de
-                        problèmes, contactez Triangle des Bermudes
+                        ©2020, Triangles des Bermudes. Tous droits réservés. En
+                        cas de problèmes, contactez Triangle des Bermudes
                     </p>
-                    <img
-                        src={triangleBermudes}
-                        style={{ zoom: "0.06" }}
-                        alt="Triangle des Bermudes"
-                    />
+                    <Navbar.Brand>
+                        <img
+                            src={triangleBermudes}
+                            style={{ zoom: 0.07 }}
+                            alt="Triangle des Bermudes"
+                        />
+                    </Navbar.Brand>
                 </div>
             </footer>
         );
