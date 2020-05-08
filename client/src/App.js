e96bd532299cc36e2d2e5493f274a1b8d0ed0bc1
Jakque <32552844+Jakque@users.noreply.github.com>
2020-05-08 18:59:09 +0200

    Page join
    added front page join

--- client/src/App.js
+++ client/src/App.js
@@ -5,33 +5,36 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap.css";
 import Home from "./components/main/static/Home";
 import Tips from "./components/main/static/Tips";
+import Join from "./components/main/static/Join";
 import Connexion from "./components/main/static/Connexion";
 import StaticFooter from "./components/templates/StaticFooter";
 import ConnectedNavbars from "./components/templates/nav/ConnectedNavbars";
 
 function App() {
-    return (
-        <div>
-            <ConnectedNavbars />
-            <Router
-                className="position-absolute"
-                style={{ left: "15%", right: "15%", overflow: "hidden" }}
-            >
-                <Switch>
-                    <Route path="/tips">
-                        <Tips />
-                    </Route>
-                    <Route path="/connexion">
-                        <Connexion />
-                    </Route>
-                    <Route path="/">
-                        <Home />
-                    </Route>
-                </Switch>
-            </Router>
-            <StaticFooter />
-        </div>
-    );
+  return (
+    <div>
+      <ConnectedNavbars />
+      <Router
+        style={{ left: "15%", right: "15%", overflow: "hidden" }}
+      >
+        <Switch>
+          <Route path="/tips">
+            <Tips />
+          </Route>
+          <Route path="/connexion">
+            <Connexion />
+          </Route>
+          <Route path="/join">
+            <Join />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+      <StaticFooter />
+    </div>
+  );
 }
 
 export default App;
