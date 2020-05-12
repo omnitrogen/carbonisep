69a9aadecb3663af2c0522de30307041275b930f
Antoine Tournedouet <antoine.tournedouet@gmail.com>
2020-05-12 14:56:34 +0200

    start quizz

---
+++ client/src/components/main/quizz.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from "react";
+import Button from 'react-bootstrap/Button';
+import JoinOrCreateGame from '../../templates/JoinOrCreateGame';
+
+export default class Quizz extends Component {
+    render() {
+        return (
+            <div className="p-3 d-flex justify-content-center pt-5">
+                <div className="border border-dark rounded w-80 p-4">
+                    <h1>Questionnaire</h1>
+
+                </div>
+            </div>
+        );
+    }
+}
