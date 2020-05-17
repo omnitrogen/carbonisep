43bc5ea9f85325a16f9545665174836f97eac404
Jakque <32552844+Jakque@users.noreply.github.com>
2020-05-17 11:12:22 +0200

    ajout de la verification du code au chargement de la page

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,31 +5,33 @@ var express = require("express");
 const router = express.Router();
 
 router.get("/hello", function getVersion(request, response) {
-  console.log("yay");
-  const row = test();
-  return response.json({
-    message: String("Queried from database" + row.test_id + " " + row.name),
-  });
+    console.log("yay");
+    const row = test();
+    return response.json({
+        message: String("Queried from database" + row.test_id + " " + row.name),
+    });
 });
 router.get("/login", function getVersion(request, response) {
-  response = model.function;
+    response = model.function;
 
-  return response.json({
-    element1: test,
-  });
+    return response.json({
+        element1: test,
+    });
 });
 
 router.get("/join", (req, res) => {
-  var code = req.query.code;
-  if (code == 12345) {
-    res.json({ exist: true });
-  }
+    var code = req.query.code;
+    if (code == 12345) {
+        res.json({ exist: true });
+    } else {
+        res.json({ exist: false });
+    }
 
-  return res;
+    return res;
 });
 
 router.get("/", (req, res) => {
-  return res.json({ message: "hehe" });
+    return res.json({ message: "hehe" });
 });
 
 module.exports = router;
