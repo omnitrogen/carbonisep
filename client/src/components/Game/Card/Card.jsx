70e05d1ad18a6a1c3d1422966c0efacd4290b693
Antoine Tournedouet <antoine.tournedouet@gmail.com>
2020-05-26 13:43:39 +0200

    front_game_methods

--- client/src/components/Game/Card/Card.jsx
+++ client/src/components/Game/Card/Card.jsx
@@ -1,22 +1,38 @@
 
 import React, { Component } from "react";
 import card_background1 from "assets/cards/background1.png";
+import card_background2 from "assets/cards/background2.png";
+import card_background3 from "assets/cards/background3.png";
+import card_background4 from "assets/cards/background4.png";
 import argent from "assets/money.png";
 
 class Card extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            background: [
+                card_background1,
+                card_background1,
+                card_background2,
+                card_background3,
+                card_background4
+            ]
+        };
+    }
+
     render() {
         return (
             <div>
                 <div className="card text-center m-3">
                     <img
                         className="card-img-top"
-                        src={card_background1}
-                        alt="Card image cap"
+                        src={this.state.background[this.props.data.background]}
+                        alt="Card image"
                     />
                     <div className="card-body">
-                        <p className="card-text">Planter un arbre</p>
+                        <p className="card-text">{this.props.data.name}</p>
                         <div className="d-flex justify-content-center">
-                            <p className="mr-2">3</p>
+                            <p className="mr-2">{this.props.data.cost}</p>
                             <img
                                 src={argent}
                                 alt="Cout en argent"
@@ -26,7 +42,7 @@ class Card extends Component {
                     </div>
                     <a
                         className="card-footer bg-info text-white font-weight-bold"
-                    // onClick={this.selectAction}
+                        onClick={() => this.props.handleSubmit(this.props.data)}
                     >
                         Choisir
                     </a>
