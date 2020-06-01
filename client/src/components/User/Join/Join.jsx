249e4c93ec6b9d3bf60b23711481bc1d4ddbea2a
omni <felixgif@gmail.com>
2020-06-01 16:41:46 +0200

    Clean Join UI

--- client/src/components/User/Join/Join.jsx
+++ client/src/components/User/Join/Join.jsx
@@ -48,7 +48,7 @@ function Join() {
     return (
         <div>
             <Navigation />
-            <Container className="d-flex justify-content-center p-5">
+            {/* <Container className="d-flex justify-content-center p-5">
                 <div className="border border-dark rounded p-4">
                     <form
                         name="form"
@@ -116,6 +116,104 @@ function Join() {
                         </div>
                     </form>
                 </div>
+            </Container> */}
+
+            <Container className="border border-dark rounded my-5 p-4">
+                <Row className="justify-content-md-center py-3">
+                    <Col lg="8">
+                        <h2 className="text-center">
+                            Can't wait to play with your friends?
+                        </h2>
+                        <h2 className="text-center">
+                            Join the game with the game code!
+                        </h2>
+                    </Col>
+                </Row>
+
+                <Row className="justify-content-md-center py-3">
+                    <Col lg="4">
+                        <form
+                            name="form"
+                            onSubmit={joinHandleSubmit}
+                            className="center"
+                        >
+                            <div className="form-group">
+                                <input
+                                    type="text"
+                                    name="code"
+                                    placeholder="Game code"
+                                    value={code}
+                                    onChange={handleChange}
+                                    className={
+                                        "form-control" +
+                                        (submittedJoin && !code
+                                            ? " is-invalid"
+                                            : "")
+                                    }
+                                />
+                                {submittedJoin && !code && (
+                                    <div className="invalid-feedback">
+                                        Code is required
+                                    </div>
+                                )}
+                            </div>
+                            <div className="form-group text-center">
+                                <button className="btn btn-success">
+                                    {joiningGame && (
+                                        <span className="spinner-border spinner-border-sm mr-1"></span>
+                                    )}
+                                    Join the game
+                                </button>
+                            </div>
+                        </form>
+                    </Col>
+                </Row>
+                <Row className="justify-content-md-center">
+                    <Col lg="6">
+                        <hr />
+                    </Col>
+                </Row>
+                <Row className="justify-content-md-center py-3">
+                    <Col lg="8">
+                        <h2 className="text-center">
+                            Or create a new game and invite all your friends!
+                        </h2>
+                    </Col>
+                </Row>
+                <Row className="justify-content-md-center py-3">
+                    <Col lg="4">
+                        <form name="form" onSubmit={createGameHandleSubmit}>
+                            <div className="form-group">
+                                <input
+                                    type="text"
+                                    name="name"
+                                    placeholder="Game name"
+                                    value={name}
+                                    onChange={handleChange}
+                                    className={
+                                        "form-control" +
+                                        (submittedCreate && !name
+                                            ? " is-invalid"
+                                            : "")
+                                    }
+                                />
+                                {submittedCreate && !name && (
+                                    <div className="invalid-feedback">
+                                        Name is required
+                                    </div>
+                                )}
+                            </div>
+                            <div className="form-group text-center">
+                                <button className="btn btn-primary">
+                                    {creatingGame && (
+                                        <span className="spinner-border spinner-border-sm mr-1"></span>
+                                    )}
+                                    Create a game
+                                </button>
+                            </div>
+                        </form>
+                    </Col>
+                </Row>
             </Container>
         </div>
     );
