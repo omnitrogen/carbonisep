f1aa234175af17e16ffad2376ac13e6c68d83a7d
omni <felixgif@gmail.com>
2020-06-02 10:54:11 +0200

    Final clean

--- client/src/components/App/Footer/Footer.jsx
+++ client/src/components/App/Footer/Footer.jsx
@@ -7,23 +7,16 @@ function Footer() {
     return (
         <footer id="footer" style={{ width: "100%" }}>
             <div
-                className="d-flex w-80 justify-content-between mt-2"
-                style={{
-                    backgroundColor: "#88B03C",
-                    alignItems: "baseline",
-                    alignContent: "center",
-                    bottom: "0",
-                    width: "100%",
-                    padding: "0.5rem",
-                }}
+                className="border border-dark w-80 d-flex justify-content-between"
+                style={{ backgroundColor: "#88B03C", height: "10vh" }}
             >
                 <Navbar.Brand href="home">
                     <img src={logo} style={{ height: 50 }} alt="logo" />
                 </Navbar.Brand>
-                <div>
+                <p>
                     ©2020, Triangles des Bermudes. Tous droits réservés. En cas
                     de problèmes, contactez Triangle des Bermudes
-                </div>
+                </p>
                 <Navbar.Brand>
                     <img
                         src={triangleBermudes}
